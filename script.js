777a059ab29993a73c3827d27243090b24eb4261
Xulian7 <jfgrossop@outlook.com>
2025-01-08 00:03:55 -0500

    Integracion diseño

--- script.js
+++ script.js
@@ -7,22 +7,37 @@ function fillRow(rowClass) {
 }
 
 // Función para realizar la multiplicación
-function multiplyNumbers() {
+function validateAndMultiply() {
     // Obtiene los números de las filas
     const row1Inputs = document.querySelectorAll('.row1');
     const row2Inputs = document.querySelectorAll('.row2');
 
-    // Convierte los inputs en un número de tres cifras
+    // Convierte los inputs de las filas en un número de tres cifras
     const number1 = Array.from(row1Inputs).map(input => input.value).join('');
     const number2 = Array.from(row2Inputs).map(input => input.value).join('');
 
-    // Valida que los números sean válidos
+    // Verifica que ambos números tengan 3 cifras y sean válidos
     if (number1.length === 3 && number2.length === 3 && !isNaN(number1) && !isNaN(number2)) {
-        const result = parseInt(number1) * parseInt(number2);
-        document.getElementById('result').textContent = result;
+        const resultFromMultiplication = parseInt(number1) * parseInt(number2);
+        
+        // Obtener los valores del input compuesto
+        const inputs = document.querySelectorAll('.input-cell.final-result');
+        let userResult = '';
+        inputs.forEach(input => {
+            const value = input.value.trim();
+            if (value !== '') {
+                userResult += value;
+            }
+        });
+
+        // Verifica si la multiplicación calculada y el resultado ingresado son iguales
+        if (userResult === resultFromMultiplication.toString()) {
+            document.getElementById('result').textContent = `¡Correcto! El resultado de la multiplicación es ${resultFromMultiplication}`;
+        } else {
+            document.getElementById('result').textContent = `Error: La multiplicación no coincide. Resultado esperado: ${resultFromMultiplication}`;
+        }
     } else {
-        // Considera personalizar el mensaje de error
-        document.getElementById('result').textContent = 'Error: Completa todos los campos con números válidos.';
+        document.getElementById('result').textContent = 'Error: Completa todos los campos con números válidos de tres cifras.';
     }
 }
 
@@ -32,7 +47,7 @@ function clearGrid() {
     inputs.forEach(input => {
         input.value = ''; // Limpiar el contenido de cada input
     });
-
+    document.getElementById('result').textContent = '';
     // Considera agregar una confirmación para evitar limpieza accidental
 }
 
@@ -50,5 +65,3 @@ function clearAux1(rowClass) {
         console.log(`No se encontraron elementos con la clase .cell-tiny.${rowClass}`);
     }
 }
-
-
